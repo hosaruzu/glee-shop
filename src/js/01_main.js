58bb039f3e06cb1c0ee9881df7cd8ebdb7262fae
Artem Tebenkov <artem.tebenkov@icloud.com>
2022-01-11 18:40:24 +0400

    Add product block

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -15,3 +15,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
   splide.mount();
 });
+
+function openTab(evt, tabName) {
+  let i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("js-tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("js-tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" tabs__btn--active", "");
+  }
+
+  document.getElementById(tabName).style.display = "grid";
+  evt.currentTarget.className += " tabs__btn--active";
+};
+
+document.getElementById("defaultOpen").click();
